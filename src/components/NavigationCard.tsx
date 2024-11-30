1eed2cea6a2f398424d5b9fcbcc64ae063633000
SebastianJedrak <jedrak.sebastian@gmail.com>
2024-11-30 12:55:59 +0100

    refactor(ui): created separate Button component

--- src/components/NavigationCard.tsx
+++ src/components/NavigationCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavigationItem from "./NavigationItem";
+import Button from "./UI/Button";
 
 const NavigationCard: React.FC = () => {
   const navigationList = [
@@ -21,12 +22,7 @@ const NavigationCard: React.FC = () => {
           <NavigationItem key={item.id} title={item.title} link={item.link} />
         ))}
       </ul>
-      <button
-        type="button"
-        className="m-4 px-4 py-2 border-2 rounded-lg border-gray-300 hover:bg-gray-100 hover:text-purple-700 focus:z-10 focus:ring-2 focus:ring-purple-700 focus:text-purple-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-purple-500 dark:focus:text-white"
-      >
-        Dodaj pozycję menu
-      </button>
+      <Button title="Dodaj pozycję menu" type="secondary" />
     </div>
   );
 };
