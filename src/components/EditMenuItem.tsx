f0b4155374833ae543df818ce30660a7c552bd67
SebastianJedrak <jedrak.sebastian@gmail.com>
2024-11-30 16:17:23 +0100

    feat(edit-menu-item): added validation

--- src/components/EditMenuItem.tsx
+++ src/components/EditMenuItem.tsx
@@ -10,8 +10,8 @@ import { FiSearch } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
 const schema = z.object({
-  menu: z.string(),
-  link: z.string(),
+  menu: z.string().min(1, "Menu is required"),
+  link: z.string().min(1, "Link is required"),
 });
 
 type FormFields = z.infer<typeof schema>;
@@ -20,8 +20,7 @@ const EditMenuItem: React.FC = () => {
   const {
     register,
     handleSubmit,
-    setError,
-    formState: { errors, isSubmitting },
+    formState: { errors },
   } = useForm<FormFields>({ resolver: zodResolver(schema) });
 
   const onSubmit: SubmitHandler<FormFields> = (data) => {
@@ -33,7 +32,7 @@ const EditMenuItem: React.FC = () => {
       <div className="flex">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="p-4 space-y-4 w-11/12"
+          className="p-4 space-y-1 w-11/12"
         >
           <div>
             <label htmlFor="menu" className="block mb-1 font-bold">
@@ -46,6 +45,9 @@ const EditMenuItem: React.FC = () => {
               {...register("menu")}
               className="w-full pl-3 py-2 border border-gray-300 rounded-lg text-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
             />
+            <p className={`text-red-500 ${errors.menu || "invisible"}`}>
+              Błąd: {errors.menu?.message}
+            </p>
           </div>
 
           <div>
@@ -58,14 +60,18 @@ const EditMenuItem: React.FC = () => {
                 id="link"
                 type="text"
                 placeholder="Wklej lub wyszukaj"
+                {...register("link")}
                 className="w-full pl-8 py-2 border border-gray-300 rounded-lg text-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
               />
             </div>
-          </div>
+            <p className={`text-red-500 ${errors.link || "invisible"}`}>
+              Błąd: {errors.link?.message}
+            </p>
 
-          <div className="flex space-x-2">
-            <Button title="Anuluj" type="button" />
-            <Button title="Dodaj" type="submit" />
+            <div className="flex space-x-2">
+              <Button title="Anuluj" type="button" />
+              <Button title="Dodaj" type="submit" />
+            </div>
           </div>
         </form>
         <div className="w-1/12 flex justify-end p-6">
