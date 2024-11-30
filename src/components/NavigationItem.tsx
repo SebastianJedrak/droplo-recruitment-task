37e0a5245025b2d0cd5107f591812fbaf288d4d4
SebastianJedrak <jedrak.sebastian@gmail.com>
2024-11-30 12:03:01 +0100

    feat(navigation-list): added button group, badge, icon to NavigationItem

--- src/components/NavigationItem.tsx
+++ src/components/NavigationItem.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React from "react";
+import { RiDragMove2Fill } from "react-icons/ri";
 
 interface NavigationItemProps {
-    key: string;
-    title: string;
-    link: string;
-  }
+  key: string;
+  title: string;
+  link: string;
+}
 
-const NavigationItem: React.FC<NavigationItemProps> = ({key, title, link}) => {
+const NavigationItem: React.FC<NavigationItemProps> = ({
+  key,
+  title,
+  link,
+}) => {
   return (
-    <li>
-      NavigationItem - {key} {title} {link}
+    <li className="flex justify-between items-center">
+      <div className="flex items-center">
+        <RiDragMove2Fill />
+        <div>
+          <div>
+            {title}
+            <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+              kolekcja
+            </span>
+          </div>
+          <div>{link}</div>
+        </div>
+      </div>
+      <div className="inline-flex rounded-md shadow-sm" role="group">
+        <button
+          type="button"
+          className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-purple-700 focus:z-10 focus:ring-2 focus:ring-purple-700 focus:text-purple-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-purple-500 dark:focus:text-white"
+        >
+          Usuń
+        </button>
+        <button
+          type="button"
+          className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-purple-700 focus:z-10 focus:ring-2 focus:ring-purple-700 focus:text-purple-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-purple-500 dark:focus:text-white"
+        >
+          Edytuj
+        </button>
+        <button
+          type="button"
+          className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-purple-700 focus:z-10 focus:ring-2 focus:ring-purple-700 focus:text-purple-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-purple-500 dark:focus:text-white"
+        >
+          Dodaj pozycję menu
+        </button>
+      </div>
     </li>
   );
 };
