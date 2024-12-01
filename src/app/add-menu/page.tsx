dd7957dac7c0493154c416d4ff9e7a2bf89cb381
SebastianJedrak <jedrak.sebastian@gmail.com>
2024-12-01 09:48:53 +0100

    feat(add-menu): prepared addForm function and rendered forms in loop

--- src/app/add-menu/page.tsx
+++ src/app/add-menu/page.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import EditMenuItem from "@/components/EditMenuItem";
 import AddMenuItem from "@/components/AddMenuItem";
 import GlobalContainer from "@/components/UI/GlobalContainer";
+import { useState } from "react";
 
-export default function Home() {
+export default function AddMenu() {
+  const [forms, setForms] = useState<{ id: number }[]>([]);
+
+  const addForm = () => {
+    setForms((forms) => [...forms, { id: forms.length }]);
+  };
+
   return (
     <GlobalContainer backgroundColor="gray">
-      <EditMenuItem />
       <AddMenuItem action="addMenu" />
+      {forms.map((form) => {
+        return (
+          <div key={form.id}>
+            <EditMenuItem />
+          </div>
+        );
+      })}
+      <button onClick={addForm}>Click</button>
     </GlobalContainer>
   );
 }
