9427a45d8948656596b0de3f7dff7eb2ea5ed99b
Sebastian Jędrak <jedrak.sebastian@gmail.com>
2024-12-02 20:34:40 +0100

    style(global): dark mode deleted

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -11,7 +11,7 @@ const Button: React.FC<ButtonProps> = ({ title, type, className, payload }) => {
   return (
     <button
       type={type}
-      className={`px-4 py-2 border-2 rounded-lg border-gray-300 hover:bg-gray-100 hover:text-purple-700 focus:z-10 focus:ring-2 focus:ring-purple-700 focus:text-purple-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-purple-500 dark:focus:text-white ${className}`}
+      className={`px-4 py-2 border-2 rounded-lg border-gray-300 hover:bg-gray-100 hover:text-purple-700 focus:z-10 focus:ring-2 focus:ring-purple-700 focus:text-purple-700  ${className}`}
       onClick={payload}
     >
       {title}
