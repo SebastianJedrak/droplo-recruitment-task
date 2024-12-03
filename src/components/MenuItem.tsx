b0f029af5776b6e8c81992b4cd388c9a94429463
Sebastian Jędrak <jedrak.sebastian@gmail.com>
2024-12-03 14:40:47 +0100

    style(dnd): changed MenuItem cursor

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -37,7 +37,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ menuItem, depth = 0 }) => {
       ref={setNodeRefDraggable}
       {...attributes}
       style={dndStyle}
-      className={`bg-white ${isDragging && "opacity-75"}`}
+      className={`bg-white ${isDragging && "opacity-75"} cursor-default`}
     >
       <div>
         <div
@@ -48,7 +48,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ menuItem, depth = 0 }) => {
             className="flex items-center"
             style={{ paddingLeft: `${depth * 16}px` }}
           >
-            <RiDragMove2Fill {...listeners} size={24} className="fill-gray-500 mr-2" />
+            <RiDragMove2Fill {...listeners} size={24} className="fill-gray-500 mr-2 cursor-pointer" />
             <div>
               <span className="mr-2 font-bold">{menuItem.label}</span>
               <div>{menuItem.url}</div>
