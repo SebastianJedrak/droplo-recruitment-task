3959ee83b5fa4d293f233861d172c6e701737141
Sebastian Jędrak <jedrak.sebastian@gmail.com>
2024-12-03 07:52:58 +0100

    style(nest-menu-itens) improved layout of MenuItem

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -10,7 +10,7 @@ interface MenuItemProps {
   depth?: number;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ menuItem, depth = 1 }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ menuItem, depth = 0 }) => {
   const { newMenuForms, addNewMenu, deleteMenuItem } = useAppContext();
 
   const filteredMenuForms = newMenuForms.filter(
@@ -18,65 +18,69 @@ const MenuItem: React.FC<MenuItemProps> = ({ menuItem, depth = 1 }) => {
   );
 
   return (
-    <div>
-      <li className="flex justify-between items-center border-b border-gray-300 px-4 py-2">
-        <div className="flex items-center">
-          <RiDragMove2Fill size={24} className="fill-gray-500 mr-2" />
-          <div>
-            <span className="mr-2 font-bold">{menuItem.label}</span>
-            <div>{menuItem.url}</div>
+    <li>
+      <div>
+        <div className="flex items-center justify-between border-b border-gray-300 px-4 py-2">
+          <div className="flex items-center" style={{ paddingLeft: `${depth * 16}px` }}>
+            <RiDragMove2Fill size={24} className="fill-gray-500 mr-2" />
+            <div>
+              <span className="mr-2 font-bold">{menuItem.label}</span>
+              <div>{menuItem.url}</div>
+            </div>
           </div>
+
+          <ButtonGroup
+            buttons={[
+              {
+                title: "Usuń",
+                type: "button",
+                payload: () => {
+                  deleteMenuItem(menuItem.id);
+                },
+              },
+              {
+                title: "Edytuj",
+                type: "button",
+                payload: () => {
+                  addNewMenu(menuItem.id, menuItem);
+                },
+              },
+              {
+                title: "Dodaj pozycję menu",
+                type: "button",
+                payload: () => {
+                  addNewMenu(menuItem.id);
+                },
+              },
+            ]}
+          />
         </div>
-        <ButtonGroup
-          buttons={[
-            {
-              title: "Usuń",
-              type: "button",
-              payload: () => {
-                deleteMenuItem(menuItem.id);
-              },
-            },
-            {
-              title: "Edytuj",
-              type: "button",
-              payload: () => {
-                addNewMenu(menuItem.id, menuItem);
-              },
-            },
-            {
-              title: "Dodaj pozycję menu",
-              type: "button",
-              payload: () => {
-                addNewMenu(menuItem.id);
-              },
-            },
-          ]}
-        />
-      </li>
-      {filteredMenuForms.length > 0 && (
-        <div className="p-4 border-b bg-gray-100 border-gray-300 space-y-4">
-          {filteredMenuForms.map((menuForm) => (
-            <EditMenuItem
-              key={menuForm.id}
-              id={menuForm.id}
-              parentId={menuItem.id}
-              menuItem={menuItem}
-            />
-          ))}
-        </div>
-      )}
-      <div style={{ paddingLeft: `${depth * 16}px` }}>
-        {menuItem.subItems &&
-          menuItem.subItems.length > 0 &&
-          menuItem.subItems.map((nestedMenuItem) => (
-            <MenuItem
-              key={nestedMenuItem.id}
-              menuItem={nestedMenuItem}
-              depth={depth + 1}
-            />
-          ))}
+
+        {filteredMenuForms.length > 0 && (
+          <div className="p-4 border-b bg-gray-100 border-gray-300 space-y-4">
+            {filteredMenuForms.map((menuForm) => (
+              <EditMenuItem
+                key={menuForm.id}
+                id={menuForm.id}
+                parentId={menuItem.id}
+                menuItem={menuItem}
+              />
+            ))}
+          </div>
+        )}
+        <ul >
+          {menuItem.subItems &&
+            menuItem.subItems.length > 0 &&
+            menuItem.subItems.map((nestedMenuItem) => (
+              <MenuItem
+                key={nestedMenuItem.id}
+                menuItem={nestedMenuItem}
+                depth={depth + 1}
+              />
+            ))}
+        </ul>
       </div>
-    </div>
+    </li>
   );
 };
 
