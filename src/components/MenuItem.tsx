f212ca2b493bf39de93686336193ec22886e8e99
Sebastian Jędrak <jedrak.sebastian@gmail.com>
2024-12-04 14:34:39 +0100

    fix(dnd): fixed border when isOver

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -45,9 +45,11 @@ const MenuItem: React.FC<MenuItemProps> = ({ menuItem, depth = 1 }) => {
       <div>
         <div
           ref={setNodeRefDroppable}
-          className={`flex items-center justify-between border-b border-gray-200 px-6 py-4 ${
-            isOver && "border-b-2 border-purple-700"
-          }`}
+          className={`flex items-center justify-between px-6 py-4 
+            ${!isOver && "border-b border-gray-200"} 
+            ${isOver && "border-b-2 border-purple-700"}
+          
+          `}
         >
           <div className="flex items-center">
             <RiDragMove2Fill
